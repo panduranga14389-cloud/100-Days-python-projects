f56f8d90273ea3af3ffa1c1a14e08a2657743480
panduranga14389-cloud <panduranga14389@gmail.com>
2026-03-14 13:47:32 +0530

    Add files via upload

---
+++ BAND NAME GENERATOR.py
@@ -0,0 +1,8 @@
+#BAND NAME GENERATOR
+print("WELCOME TO THE BAND NAME GENERATOR!")
+city=input("Which city you grow up in?\n")
+pet=input("Which is your favourite pet?\n")
+band_name=city+" "+pet
+print("Your band name could be:",band_name)
+           
+
